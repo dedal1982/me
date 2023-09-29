d2229c5eccf3640468bce254b17582c1bfc5d8f6
AlDm <d44.a1@yandex.ru>
2023-09-29 19:54:42 +0300

    2.6

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,42 +1,42 @@
-const swiper = new Swiper(".hero__swiper", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: false,
-  slidesPerView: 4,
+// const swiper = new Swiper(".hero__swiper", {
+//   // Optional parameters
+//   direction: "horizontal",
+//   loop: false,
+//   slidesPerView: 4,
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
+//   // If we need pagination
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".hero__swiper-button-next",
-    prevEl: ".hero__swiper-button-prev",
-  },
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: ".hero__swiper-button-next",
+//     prevEl: ".hero__swiper-button-prev",
+//   },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-  breakpoints: {
-    // when window width is >= 320px
-    375: {
-      slidesPerView: 2,
-      spaceBetween: 10,
-    },
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: ".swiper-scrollbar",
+//   },
+//   breakpoints: {
+//     // when window width is >= 320px
+//     375: {
+//       slidesPerView: 2,
+//       spaceBetween: 10,
+//     },
 
-    768: {
-      slidesPerView: 4,
-      spaceBetween: 10,
-    },
-    1200: {
-      slidesPerView: 3,
-      spaceBetween: 10,
-    },
-    1440: {
-      slidesPerView: 4,
-      spaceBetween: 20,
-    },
-  },
-});
+//     768: {
+//       slidesPerView: 4,
+//       spaceBetween: 10,
+//     },
+//     1200: {
+//       slidesPerView: 3,
+//       spaceBetween: 10,
+//     },
+//     1440: {
+//       slidesPerView: 4,
+//       spaceBetween: 20,
+//     },
+//   },
+// });
