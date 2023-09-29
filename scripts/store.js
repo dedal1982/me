ac80981b6649ed3fadc3bb2fd9acc864bd54ecf5
AlDm <d44.a1@yandex.ru>
2023-09-29 20:27:35 +0300

    2.8

---
+++ scripts/store.js
@@ -0,0 +1,41 @@
+/***открытие бургера***/
+const burgerClick = document.querySelector(".header__burger");
+const scrollLock = document.querySelector(".page");
+const menuMobile = document.querySelector(".header__mobile");
+
+burgerClick.addEventListener("click", () => {
+  burgerClick.classList.toggle("active");
+  menuMobile.classList.toggle("header__mobile_open");
+  scrollLock.classList.toggle("lock");
+});
+
+const linksNavMenu = document.querySelectorAll(".header__link");
+
+function toggleClass() {
+  linksNavMenu.forEach((el) => {
+    el.classList.remove("active");
+  });
+
+  this.classList.add("active");
+}
+
+linksNavMenu.forEach((el) => {
+  el.addEventListener("click", toggleClass);
+});
+
+/*липкий хедер*/
+window.onscroll = function showHeader() {
+  let header = document.querySelector(".header__wrapper");
+  let topHeaderWrapper = document.querySelector(".header__nav");
+  let burgerFixed = document.querySelector(".header__burger");
+
+  if (window.pageYOffset > 200) {
+    header.classList.add("header__fixed");
+    topHeaderWrapper.classList.add("header__nav-scroll");
+    burgerFixed.classList.add("header__burger-fixed");
+  } else {
+    header.classList.remove("header__fixed");
+    topHeaderWrapper.classList.remove("header__nav-scroll");
+    burgerFixed.classList.remove("header__burger-fixed");
+  }
+};
