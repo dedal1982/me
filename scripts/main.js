fd5a57f719c74c15f462142ce11ef895ffbda3ea
AlDm <d44.a1@yandex.ru>
2023-09-30 01:16:21 +0300

    3.1

--- scripts/main.js
+++ scripts/main.js
@@ -26,16 +26,13 @@ linksNavMenu.forEach((el) => {
 /*липкий хедер*/
 window.onscroll = function showHeader() {
   let header = document.querySelector(".header__wrapper");
-  let topHeaderWrapper = document.querySelector(".header__nav");
   let burgerFixed = document.querySelector(".header__burger");
 
   if (window.pageYOffset > 200) {
     header.classList.add("header__fixed");
-    topHeaderWrapper.classList.add("header__nav-scroll");
     burgerFixed.classList.add("header__burger-fixed");
   } else {
     header.classList.remove("header__fixed");
-    topHeaderWrapper.classList.remove("header__nav-scroll");
     burgerFixed.classList.remove("header__burger-fixed");
   }
 };
